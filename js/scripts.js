a25cd183cd7bb4cc33177dced72b58fc7c6da629
jeffrey muriithi <jeffreyem99@gmail.com>
2019-06-17 16:24:47 +0300

    Cost functionality

--- js/scripts.js
+++ js/scripts.js
@@ -5,40 +5,44 @@ function pizza(size, crust, toppings) {
   this.pizzaTops = toppings;
   this.pizzaPrice = 0;
 }
-pizza.prototype.price(){
-  var grandTotal = [];
-  var pizzaSize = ["Large", "Medium", "Small"];
-  var pizzaCrust = ["Crispy", "Stuffed", "Gluten-Free"];
-  var pizzaTops = ["Pepperoni", "Extra-Cheese", "Green-pepper", "Black-Olives"];
-  /**pizza size to pizza sizes */
+var cost = [];
+var pizzaSize = ["Large", "Medium", "Small"];
+var pizzaCrust = ["Crispy", "Stuffed", "Gluten-Free"];
+var pizzaTops = ["Pepperoni", "Extra-Cheese", "Green-pepper", "Black-Olives"];
+
+/**pizza size to pizza sizes */
+pizza.prototype.price = function () {
   if (this.pizzaSize === pizzaSize[0]) {
-    pizzaPrice = +1000;
+    pizzaPrice +=1000;
   }
   else if (this.pizzaSize === pizzaSize[1]) {
-    pizzaPrice = +800;
+    pizzaPrice +=800;
   }
   else if (this.pizzaSize === pizzaSize[2]) {
-    pizzaPrice = +600;
+    pizzaPrice +=600;
   }
   /*pizza crust to pizza size to pizza price */
   if (this.pizzaCrust === pizzaCrust[0]) {
-    pizzaPrice = +150;
+    pizzaPrice += 150;
   }
   else if (this.pizzaCrust === pizzaCrust[1]) {
-    pizzaPrice = +100;
+    pizzaPrice += 100;
   }
   else if (this.pizzaCrust === pizzaCrust[2]) {
-    pizzaPrice = +50;
+    pizzaPrice += 50;
   }
   /**pizza size to pizza toppings */
-  if (this.pizzaTops === pizzaSize[0]) {
-    pizzaPrice = +150;
+  if (this.pizzaTops === pizzaTops[0]) {
+    pizzaPrice += 150;
   }
-  else if (this.pizzaTops === pizzaSize[1]) {
-    pizzaPrice = +100;
+  else if (this.pizzaTops === pizzaTops[1]) {
+    pizzaPrice += 100;
   }
-  else if (this.pizzaTops === pizzaSize[2]) {
-    pizzaPrice = +50;
+  else if (this.pizzaTops === pizzaTops[2]) {
+    pizzaPrice += 50;
+  }
+  else if (this.pizzaTops === pizzaTops[3]) {
+    pizzaPrice += 50;
   }
   return this.pizzaPrice;
 }
@@ -50,37 +54,32 @@ pizza.prototype.deliveryCost = function () {
 pizza.prototype.grandTotal = function () {
   var checkoutCartTotal = 0;
 
-  for (var order = 0; order < grandTotal.length; order++) {
-    checkoutCartTotal = + grandTotal[order];
+  for (var order = 0; order < cost.length; order++) {
+    checkoutCartTotal += cost[order];
   }
   return checkoutCartTotal;
 }
 
 $(document).ready(function () {
-  $("placeOrder").click(function (e) {
+  $("#placeOrder").click(function (e) {
     e.preventDefault();
-    var sizes = $("select.sizes").change(function (e) {
-      var selectedSize = $(this).children("option:selected").val();
-      e.preventDefault();
-    });
-    var crusts = $("select.crusts").change(function (e) {
-      var selectedCrust = $(this).children("option:selected").val();
-      e.preventDefault();
 
-    });
-    var tops = $("select.tops").change(function (e) {
-      var selectedTops = $(this).children("option:selected").val();
-      e.preventDefault();
 
-    });
-    var newPizzas = new pizza(sizes,crusts,tops);
-    newPizzas.pizzaPrice();
-    $(".sizey").text("1" + sizes);
-    $(".crustey").text("2" + crusts);
-    $(".toopie").text("3" + tops);
-    $(".costie").text("4"+ grandTotal);
+
+    var sizes = $("select.sizes").val();
+    var crusts = $("select.crusts").val();
+    var tops = $("select.tops").val();
+
+
+    var newPizzas = new pizza(sizes, crusts, tops);
+    newPizzas.price();
+    cost.push(newPizzas.pizzaPrice)
+    $(".sizey").text("Sizes: " + sizes);
+    $(".crustey").text("Crusts: " + crusts);
+    $(".toopie").text("Toppings: " + tops);
+    $(".costie").text("Cost: " + newPizzas.grandTotal());
   });
-}); 
+});
   // size = $("select.sizes").change(function () {
   //   var selectedSize = $(this).children("option:selected").val();
   //   $("ul").append("<li><span>" + selectedSize + "</span></li>");
